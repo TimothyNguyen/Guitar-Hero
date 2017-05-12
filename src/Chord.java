7ce79c8ea02ae17701667b13c179234802644294
Timothy Nguyen <quynhthoa1972@gmail.com>
2017-05-12 14:07:45 -0400

    Create Chord.java

---
+++ src/Chord.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Chord {
+
+	private ArrayList<MusicNote> chord;
+	
+	public Chord(ArrayList<MusicNote> chord) {
+		chord = this.chord;
+	}
+	
+	public Chord(MusicNote note) {
+		chord.add(note);
+	}
+
+}
