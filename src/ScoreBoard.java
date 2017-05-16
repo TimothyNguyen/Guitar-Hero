786c3d68603cce257b1174f391f24b03f63463d8
Quan Thi <quanthi360@gmail.com>
2017-05-16 14:28:33 -0400

    Jlabel

--- src/ScoreBoard.java
+++ src/ScoreBoard.java
@@ -11,25 +11,30 @@ public class ScoreBoard extends JPanel{
 	private int width, height;
 	private Rectangle scoreBoard;
 	private Color color;
-	private JLabel title;
+
+	private JLabel title = new JLabel("SCORE");
 	
 	
 	public ScoreBoard(){
 		this.width = 275;
-		this.height = 200;
+		this.height = 300;
 		this.xCoor = GamePanel.getBoardWidth() - GamePanel.getBoardWidth()/3;
-		this.yCoor= GamePanel.getBoardHeight() - GamePanel.getBoardHeight()/3;
+		this.yCoor= GamePanel.getBoardHeight() - 400;
 		this.color = new Color(153, 76, 0);
+		
 		scoreBoard = new Rectangle(xCoor, yCoor, width, height);
 		
-		title = new JLabel("Score");
+		title.setBounds(xCoor, yCoor, 20, 20);
+		title.setOpaque(true);
 		add(title);
+
 		
 	}
 	
 	public void render(Graphics2D g){
 		g.setColor(color);
 		g.fill(scoreBoard);
+		
 		
 	}
 }
